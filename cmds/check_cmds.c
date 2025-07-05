34491fcb6acf1703a378f149938a4f4562c740e4
kaitoxicc <kaito@Garganal.>
2025-07-05 17:18:41 +0200

    e

--- cmds/check_cmds.c
+++ cmds/check_cmds.c
@@ -32,29 +32,23 @@ void	free_args(char **args)
 	free(args);
 }
 
-int	builtin_exec(char *user_input, t_envp **datas, t_history *history)
+int	builtin_exec(t_cmd_info *cmd_info, t_envp **datas, t_history *history)
 {
-	char	**args;
-	int		total_args;
-
-	args = ft_split(user_input, ' ');
-	total_args = args_counter(args);
-	if (ft_strncmp(user_input, "cd", 2) == 0)
-		cd(total_args, args[1]);
-	else if (ft_strncmp(user_input, "echo", 4) == 0)
-		echo(total_args, (const char **)args);
-	else if (ft_strncmp(user_input, "env", 3) == 0)
+	if (ft_strncmp(cmd_info->cmd, "cd", 2) == 0)
+		cd(cmd_info->argc, cmd_info->argv[1]);
+	else if (ft_strncmp(cmd_info->cmd, "echo", 4) == 0)
+		echo(cmd_info->argc, (const char **)cmd_info->argv);
+	else if (ft_strncmp(cmd_info->cmd, "env", 3) == 0)
 		env(*datas);
-	else if (ft_strncmp(user_input, "export", 6) == 0)
-		export(total_args, args, datas);
-	else if (ft_strncmp(user_input, "unset", 5) == 0)
-		unset(total_args, args, datas);
-	else if (ft_strncmp(user_input, "history", 7) == 0)
+	else if (ft_strncmp(cmd_info->cmd, "export", 6) == 0)
+		export(cmd_info->argc, cmd_info->argv, datas);
+	else if (ft_strncmp(cmd_info->cmd, "unset", 5) == 0)
+		unset(cmd_info->argc, cmd_info->argv, datas);
+	else if (ft_strncmp(cmd_info->cmd, "history", 7) == 0)
 		print_history(history);
-	else if (ft_strncmp(user_input, "exit", 4) == 0)
+	else if (ft_strncmp(cmd_info->cmd, "exit", 4) == 0)
 		ft_exit();
 	else
 		return (0);
-	free_args(args);
 	return (1);
 }
